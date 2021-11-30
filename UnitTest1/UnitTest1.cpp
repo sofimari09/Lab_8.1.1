5311c9ebd7b3d4f0f6d0f29d06759b870032f52f
unknown <Desofiyamariya@gmail.com>
2021-12-01 00:34:35 +0200

    initial commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../8.1.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char str[] = "abc";
+			int m = Count(str);
+				Assert::AreEqual(m, 1);
+		}
+	};
+}
